47b1faf3e37c374b3fcf20a91c9a84531bdcb4a2
Jonah Unuafe <fejisfejiro@gmail.com>
2023-07-19 15:36:07 +0100

    new features

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
+"use strict";
 
-  document.querySelectorAll(".question").forEach((question) => question.addEventListener("click", () => {
+document.querySelectorAll(".question").forEach((question) => question.addEventListener("click", () => {
 
-    if(question.parentNode.classList.contains("active")) {
-      question.parentNode.classList.toggle("active")
-    }
-    else {
-      document.querySelectorAll(".question").forEach(question => question.parentNode.classList.remove("active"))
-      question.parentNode.classList.add("active");
-    }
-  }));
+  if(question.parentNode.classList.contains("active")) {
+    question.parentNode.classList.toggle("active")
+  } else {
+    document.querySelectorAll(".question").forEach(question => question.parentNode.classList.remove("active"))
+    question.parentNode.classList.add("active");
+  }
+  
+}));
+
+
+document.addEventListener("click", (event) => {
+  const questionsAnswerAccordian = document.querySelectorAll(".question");
+
+  if (!event.target.closest(".question")) {
+  questionsAnswerAccordian.forEach((question) => {
+    if (question.parentNode.classList.contains("active")) {
+      question.parentNode.classList.remove("active");
+      }
+    });
+  }
+});
+
+
+
